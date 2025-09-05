ab1795bcaadfc637821a1073461a1de7d599915a
antoninofoti <antonyf99@gmail.com>
2025-09-05 13:39:03 +0200

    fix server error on auth schema update

--- init-scripts/01-auth-schema.sql
+++ init-scripts/01-auth-schema.sql
@@ -3,7 +3,8 @@ CREATE TABLE credentials (
     id SERIAL PRIMARY KEY,
     username VARCHAR(255) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL,
-    registration_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    registration_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    role VARCHAR(50) DEFAULT 'developer'
 );
 
 -- fix_this_stuff_system table (a global system config shared here for simplicity)
